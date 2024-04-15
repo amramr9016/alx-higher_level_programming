6b8345b3d44a3d940e005ee24ac3bd7bf9fa7e13
3mr <amramr9016@gmail.com>
2024-04-15 10:07:37 -0700

    advanced

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
+"""
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    state_obj = State(name='California')
+    city_obj = City(name='San Francisco')
+    state_obj.cities.append(city_obj)
+
+    session.add(state_obj)
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Lists all State objects and corresponding City objects contained in the DB
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+
+    for state_obj in states:
+        print("{}: {}".format(state_obj.id_num, state_obj.state_name))
+        for city_obj in state_obj.cities:
+            print("    {}: {}".format(city_obj.id_num, city_obj.city_name))
