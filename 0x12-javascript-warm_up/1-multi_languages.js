17ac206fb33847b70e330f68bc8fed97eca61b12
3mr <amramr9016@gmail.com>
2024-04-15 07:09:06 -0700

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const fristLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+console.log(fristLine);
+console.log(secondLine);
+console.log(thirdLine);
